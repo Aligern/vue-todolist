0edf1f2ee47af1ed998bcfb3872248b3af4cbf70
Aligern <sasso.luca93@gmail.com>
2024-04-02 14:12:28 +0200

    first commit

---
+++ Js/data.js
@@ -0,0 +1,27 @@
+export const starShip = [
+    {
+        id: 1,
+        text: 'Quantum engine',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'Plasma rifles',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'Scanner',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'Energy Shield',
+        done: false
+    },
+    {
+        id: 5,
+        text: 'Quantum engine',
+        done: false
+    },
+]
---
+++ Js/script.js
@@ -0,0 +1,51 @@
+import { starShip } from "./data.js";
+
+// we recall Vue:
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            starShip: starShip,
+            componentText: ''
+        }
+    },
+    methods: {
+        toggleDone(id) {
+            const newItem = this.starShip.find((el) => {
+                return el.id === id;
+            })
+            console.log(newItem);
+            if (newItem) {
+                newItem.done = !newItem.done;
+            }
+        },
+        removeItem(id) {
+            const i = this.starShip.findIndex((el) => el.id === id);
+            if (i !== -1) {
+                this.starShip.splice(i,1);
+            }
+        },
+        addComponent() {
+            const newComponent = {
+                id: null,
+                text: this.componentText,
+                done: false
+            }
+            let nextId = 0;
+            this.starShip.foreach((el) => {
+                if(nextId < el.id) {
+                    nextId = el.id;
+                }
+            });
+            newComponent.id= nextId + 1;
+            this.starShip.push(newComponent);
+            this.componentText = '';
+            console.log(this.starShip);
+        }
+
+    },
+    mounted() {
+
+    }
+}).mount('#app');
